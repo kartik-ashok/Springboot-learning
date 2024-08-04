a6479dbe33bd7476eebe7b3972896bebd2199e54
Kartik Ashok Katti <kartikkatti55@gmail.com>
2024-08-04 23:17:45 +0530

    database connection completed

--- src/main/java/com/api/book/bootrestbook/controllers/BookControllers.java
+++ src/main/java/com/api/book/bootrestbook/controllers/BookControllers.java
@@ -1,16 +1,18 @@
 package com.api.book.bootrestbook.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.book.bootrestbook.entities.Book;
@@ -24,21 +26,16 @@ public class BookControllers {
         @Autowired
         private BookService bookService;
 
-
-
-
+        
         @GetMapping("/book")
-        public List<Book> getBooks(){
-            // Book book=new Book();
-            // book.setId(101);
-            // book.setTitle("Mother");
-            // book.setAuthor("God");
-            // In Java sprinboot automatially convert object into json and return
-            // this is Because of One dependency i.e jackson
-            // object to json and json to Object
-            // return book;
+        public ResponseEntity<List<Book>> getBooks(){
             List<Book> allBooks=bookService.getAllBooks();
-            return allBooks;
+            if(allBooks.size()<=0){
+    
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+                // build method will create a responseEntity Object;
+            }
+            return ResponseEntity.of(Optional.of(allBooks));
             
         }
 
--- src/main/java/com/api/book/bootrestbook/service/BookService.java
+++ src/main/java/com/api/book/bootrestbook/service/BookService.java
@@ -4,51 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.api.book.bootrestbook.dao.BookRepo;
 import com.api.book.bootrestbook.entities.Book;
 
 @Component
 public class BookService {
-    private static List<Book> list=new ArrayList<>();
-    static {
-
-        list.add(new Book(0, "abc", "xyz"));
-        list.add(new Book(1, "cba", "zyx"));
-        list.add(new Book(2, "bca", "yzx"));
-        list.add(new Book(3, "cab", "zay"));
-    }
+    @Autowired
+    private BookRepo bookRepo;
+    
     public List<Book> getAllBooks(){
-        return list;
+       List<Book> listOfBooks= (List<Book>) this.bookRepo.findAll();
+        return listOfBooks;
     }
 
      public Book getSingleBooks(int id){
-        Book singleBook=list.stream().filter(e->e.getId()==id).findFirst().get();
-        return singleBook;
+        Book book=null;
+        try {
+      book=  this.bookRepo.findById(id);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return book;
     }
 
     public Book addBook(Book book){
-            list.add(book);
-            return book;
+            // list.add(book);
+           Book savedBook= this.bookRepo.save(book);
+            return savedBook;
     }
 
     
-    public Book deleteBook(int bookId){
-         Book deletedBook=list.stream().filter(book->book.getId()==bookId).findFirst().get();
-            return deletedBook;
+    public void deleteBook(int bookId){
+        this.bookRepo.deleteById(bookId);
     }
 
 
         public void updateBook(Book book,int bookId){
-          list =  list.stream().map(e->{
-                if (e.getId()==bookId) {
-                    e.setTitle(book.getTitle());
-                    e.setAuthor(book.getAuthor());
-                    
-                }
-                return book;
-            }).collect(Collectors.toList());
-            // return deletedBook;
+            book.setId(bookId);
+        this.bookRepo.save(book);
     }
 
 
